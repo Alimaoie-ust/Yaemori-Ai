284791723d0f31182f9897c53e21829e49ce22af
ALI Maoie <alimaoie2@gmail.com>
2026-01-15 02:10:41 +0100

    Add plugins/main•templatem.js

---
+++ plugins/main•templatem.js
@@ -0,0 +1,23 @@
+let handler = async (m, { text, usedPrefix, command }) => {
+  let user = global.db.data.users[m.sender]
+  let input = text.trim().toLowerCase()
+
+  if (input === 'list') {
+    user.menuMode = 'list'
+    m.reply('✅ تم تفعيل وضع القائمة (LIST MENU)')
+  } else if (input === 'gif' || input === 'normal') {
+    user.menuMode = 'normal'
+    m.reply('✅ تم تفعيل الوضع العادي (NORMAL MENU)')
+  } else {
+    // رسالة الخطأ في حال كتابة أمر غير صحيح
+    m.reply(`⚠️ أمر غير صالح!\n\nاستخدم:\n📌 *${usedPrefix}${command} list* (لوضع القائمة)\n📌 *${usedPrefix}${command} gif* (لوضع الفيديو/الصوت)`)
+  }
+}
+
+handler.help = ['temp']
+handler.arabic = ['غير <list/gif']
+handler.tags = ['main']
+handler.command = ['temp','غير'] 
+handler.register = true
+
+export default handler
