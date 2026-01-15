d6f0718a8940a1d9b856c88f508cc1798080a599
ALI Maoie <alimaoie2@gmail.com>
2026-01-15 02:10:45 +0100

    Add plugins/owner•reactchannel.js

---
+++ plugins/owner•reactchannel.js
@@ -0,0 +1,32 @@
+/*
+FEATURE: REACT TO WHATSAPP CHANNEL 😹
+*/
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw ("Please enter a WhatsApp channel link in the correct format. exemple :\n\n*.reactchannel* https://whatsapp.com/channel/0029Vb71THB0bIdswhCzVJ0f/475");
+
+  const match = text.match(/https:\/\/whatsapp\.com\/channel\/(\w+)(?:\/(\d+))?/);
+  if (!match) throw ("Invalid URL. Please double-check.");
+
+  const channelId = match[1];
+  const chatId = match[2];
+  if (!chatId) throw ("Chat ID not found in the provided link.");
+
+  // Your default channel ID
+  const defaultChannelId = "YOU WHATSAPP CHANNEL ID 🤗😅";
+
+  conn.newsletterMetadata("invite", channelId || defaultChannelId).then(data => {
+    if (!data) throw ("Newsletter not found or an error occurred.");
+
+    conn.newsletterReactMessage(data.id, chatId, text.split(" ").slice(1).join(" ") || "😋✅🤣🥳🤣🔥🤣🔥🤣");
+  });
+
+  m.reply("Success");
+}
+
+handler.help = ['reactchannel'];
+handler.arabic = ['رياكت']
+handler.command = ['reactchannel','رياكت'];
+handler.tags = ['owner'];
+handler.owner = true
+export default handler;
