84f2db712634e05307b88747c40b22b10af9480f
ALI Maoie <alimaoie2@gmail.com>
2026-01-26 10:49:40 +0100

    Add files via upload

--- plugins/group•config.js
+++ plugins/group•config.js
@@ -82,6 +82,7 @@ let handler = async (m, { conn, args, usedPrefix, command, isAdmin }) => {
 
 handler.help = ['protection']
 handler.arabic = ['حماية']
+handler tags = ['group']
 handler.command = /^(حماية|protection)$/i
 handler.admin = true
 handler.group = true
